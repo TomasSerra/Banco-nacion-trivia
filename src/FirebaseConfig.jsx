803aef7bbd2a322e83cbaddee598109291893b65
Tomás Serra <tomi.serra@gmail.com>
2025-12-30 19:55:47 -0300

    New Firebase Project

--- src/FirebaseConfig.jsx
+++ src/FirebaseConfig.jsx
@@ -4,11 +4,12 @@ import 'firebase/database';
 // Configura Firebase con la configuración de tu proyecto
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "trivia-banco-nacion.firebaseapp.com",
-  projectId: "trivia-banco-nacion",
-  storageBucket: "trivia-banco-nacion.appspot.com",
-  messagingSenderId: "631796970978",
-  appId: "1:631796970978:web:48c3ecce793c9b1c6cf5bb"
+  authDomain: "nacionseguros-3662a.firebaseapp.com",
+  databaseURL: "https://nacionseguros-3662a-default-rtdb.firebaseio.com",
+  projectId: "nacionseguros-3662a",
+  storageBucket: "nacionseguros-3662a.firebasestorage.app",
+  messagingSenderId: "961455310490",
+  appId: "1:961455310490:web:78a0eff31354e1b3ffc123"
 };
 
 const app = initializeApp(firebaseConfig);
